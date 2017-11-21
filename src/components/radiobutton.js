9d68ab9845497c13a5ceb808baa09471034b9674
bdinh <baodinh96@gmail.com>
2017-11-21 03:20:35 -0800

    Refactor componenents and made util functions to export

---
+++ src/components/radiobutton.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+// Group of radio button components created and binned to values of an array
+export default class RadioButton extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+
+        const {
+            valueArray,
+            updateVisualCallback,
+        } = this.props;
+
+        return (
+            <div className="btn-group btn-container" data-toggle="buttons">
+                {valueArray.map((value, i) => {
+                    return (<label key={i} className="btn btn-primary small visual-options">
+                        <input type="radio" name={value} value={value} onClick={(event) => {
+                            updateVisualCallback(event.currentTarget.value)
+                        }}/> {value}
+                    </label>)
+                })}
+
+            </div>
+        );
+    }
+}
